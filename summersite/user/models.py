650d468b85b163e7c5ce2f7c3ceeecfb74a895e4
liuxinyu1984 <liuxinyu1984@users.noreply.github.com>
2023-04-10 21:55:24 -0700

    Create and migrate new user model 'MyUser' created, extending the built-in User model with new field 'wechat_id'.

--- summersite/user/models.py
+++ summersite/user/models.py
@@ -1,3 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class MyUser(AbstractUser):
+    wechat_id = models.CharField(max_length=20, blank=False, unique=True)
