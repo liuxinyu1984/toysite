886a5d2561f2fc69218447e2be1c5a7c62d48ac6
liuxinyu1984 <liuxinyu1984@users.noreply.github.com>
2023-04-25 16:57:18 -0700

    add login_required decorator to my_classes view

--- summersite/teachings/views.py
+++ summersite/teachings/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from courses.models import Course, Lecture, UploadNote, UploadVideo
 
 
+@login_required(login_url="/users/login/")
 def my_classes(request):
     my_courses = Course.objects.filter(instructor=request.user)
     return render(
