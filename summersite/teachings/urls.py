48c921a13b516a26ba84012eaeacbb8c96a93703
liuxinyu1984 <liuxinyu1984@users.noreply.github.com>
2023-04-26 11:03:56 -0700

    add lecture notes/videos to course summary page

--- summersite/teachings/urls.py
+++ summersite/teachings/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.my_classes, name='my_classes'),
+    path('<int:course_id>/summary', views.course_summary, name='course_summary'),
 ]
--- summersite/teachings/views.py
+++ summersite/teachings/views.py
@@ -1,16 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from courses.models import Course, Lecture, UploadNote, UploadVideo
 
 
 @login_required(login_url="/users/login/")
 def my_classes(request):
-    my_courses = Course.objects.filter(instructor=request.user)
+    my_courses = Course.objects.filter(
+        instructor=request.user).order_by('subject', 'course_number')
     return render(
         request,
         'my_classes.html',
         {
             'my_courses': my_courses,
             'user': request.user,
+        }
+    )
+
+
+def course_summary(request, course_id):
+    this_course = get_object_or_404(Course, pk=course_id)
+    lectures = Lecture.objects.filter(
+        course=this_course).order_by('week', 'created_at')
+    return render(
+        request,
+        'single_class.html',
+        {
+            'course': this_course,
+            'lectures': lectures,
         }
     )
